eeeb39b093bf64b13665bab70ff10cc437e48661
Parker <pg.allen007@gmail.com>
2020-02-14 10:48:34 -0600

    Initial Commit

---
+++ PokemonLookup/PokedexDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokedexDetailViewController.swift
+//  PokemonLookup
+//
+//  Created by admin on 2/14/20.
+//  Copyright © 2020 HolyDiver. All rights reserved.
+//
+
+import UIKit
+
+class PokedexDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PokemonLookup/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PokemonLookup
+//
+//  Created by admin on 2/14/20.
+//  Copyright © 2020 HolyDiver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //https:pokeapi.co/api/v2/pokemon
+
+}
+
